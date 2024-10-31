4b0785229b8afcb953bdfe0a8bd44f76147c640d
sajjad0057 <sajjadhossain0057@gmail.com>
2024-11-01 02:38:38 +0600

    add some learing picture about redux flow and start using redux toolkit

--- rrt/src/components/Counter.jsx
+++ rrt/src/components/Counter.jsx
@@ -7,7 +7,6 @@ const Counter = ({count, onIncrement, onDecrement}) => {
 
   //console.log(count, onIncrement, onDecrement);
   
-
   return (
     <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-slate-200 rounded shadow">
       <Count  countValue = {count}/>
