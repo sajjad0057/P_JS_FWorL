d6c55f46c48a059cf36c673447a510dd5c188144
sajjad0057 <sajjadhossain0057@gmail.com>
2024-10-31 16:23:30 +0600

    init project and configure tailwind.css

---
+++ rrt/src/App.jsx
@@ -0,0 +1,8 @@
+// Example: App.js
+export default function App() {
+	return (
+		<div className="flex items-center justify-center min-h-screen bg-gray-100">
+			<h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+		</div>
+	);
+}
