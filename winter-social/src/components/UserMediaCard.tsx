8ed39baa3953073f9c4053b2468334e85eb38376
sajjad0057 <sajjadhossain0057@gmail.com>
2024-11-05 16:00:29 +0600

    Completed UserMediaCard Components

--- winter-social/src/components/UserMediaCard.tsx
+++ winter-social/src/components/UserMediaCard.tsx
@@ -12,6 +12,72 @@ const UserMediaCard = ({ userId }: { userId?: string }) => {
 					See All
 				</Link>
 			</div>
+			{/* BOTTOM  */}
+			<div className="flex gap-4 justify-evenly flex-wrap">
+				<div className="relative w-1/5 h-24">
+					<Image
+						src="/img/colorful_leaf.jpg"
+						alt="img"
+						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+						fill
+						className="object-cover rounded-md"
+					/>
+				</div>
+				<div className="relative w-1/5 h-24">
+					<Image
+						src="/img/female_1.jpg"
+						alt="img"
+						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+						fill
+						className="object-cover rounded-md"
+					/>
+				</div>
+				<div className="relative w-1/5 h-24">
+					<Image
+						src="/img/colors.png"
+						alt="img"
+						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+						fill
+						className="object-cover rounded-md"
+					/>
+				</div>
+				<div className="relative w-1/5 h-24">
+					<Image
+						src="/img/girl_3.jpg"
+						alt="img"
+						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+						fill
+						className="object-cover rounded-md"
+					/>
+				</div>
+				<div className="relative w-1/5 h-24">
+					<Image
+						src="/img/colorfull_bird.jpg"
+						alt="img"
+						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+						fill
+						className="object-cover rounded-md"
+					/>
+				</div>
+				<div className="relative w-1/5 h-24">
+					<Image
+						src="/img/girl_4.jpg"
+						alt="img"
+						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+						fill
+						className="object-cover rounded-md"
+					/>
+				</div>
+				<div className="relative w-1/5 h-24">
+					<Image
+						src="/img/girl_2.jpg"
+						alt="img"
+						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+						fill
+						className="object-cover rounded-md"
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
